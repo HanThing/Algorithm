ae42e2928d0cfa6d031d196437c9ba63f8843a25
HanThing <cjfdndgks1@gmail.com>
2025-01-29 02:12:31 +0900

    10. 가장 짧은 문자거리

--- src/String/ex1.java
+++ src/String/ex1.java
@@ -1,4 +1,36 @@
 package String;
+import java.util.*;
 
 public class ex1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        char t = sc.next().charAt(0);
+        char[] arr = str.toCharArray();
+        int front=0, rear=0;
+
+
+
+        for (int i = 0; i < arr.length; i++) {
+            int count=0;
+            front = rear = i;
+            if(i!=0)
+                System.out.print(" ");
+            while(front>=0 && rear<=arr.length-1) {
+                if(arr[front]==t) {
+                    System.out.print(count);
+                    break;
+                }
+                else if(arr[rear]==t) {
+                    System.out.print(count);
+                    break;
+                }
+                if(front > 0)
+                    front--;
+                if(rear < arr.length-1)
+                    rear++;
+                count++;
+            }
+        }
+    }
 }
