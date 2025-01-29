8811d83e23efa4f5ce59a999026a375b9a246694
HanThing <cjfdndgks1@gmail.com>
2025-01-30 00:31:51 +0900

    11. 문자열 압축 설명
    알파벳 대문자로 이루어진 문자열을 입력받아 같은 문자가 연속으로 반복되는 경우 반복되는
    
    문자 바로 오른쪽에 반복 횟수를 표기하는 방법으로 문자열을 압축하는 프로그램을 작성하시오.
    
    단 반복횟수가 1인 경우 생략합니다.
    
    입력
    
    첫 줄에 문자열이 주어진다. 문자열의 길이는 100을 넘지 않는다.
    
    출력
    
    첫 줄에 압축된 문자열을 출력한다.
    
    예시 입력 1
    
    KKHSSSSSSSE
    예시 출력 1
    
    K2HS7E

---
+++ src/String/ex3.java
@@ -0,0 +1,24 @@
+package String;
+
+import java.util.*;
+
+public class ex3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        char[] arr = str.toCharArray();
+        //char[] result = new char[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            int count=1;
+            System.out.print(arr[i]);
+            while(i<arr.length-1 && arr[i] == arr[i+1]) {
+                count++;
+                i++;
+            }
+            if(count > 1)
+                System.out.print(count);
+
+        }
+    }
+}
