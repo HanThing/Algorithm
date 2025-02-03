cbe084b3601f6d6237edc311933de12f1304bd7e
HanThing <cjfdndgks1@gmail.com>
2025-02-03 23:16:12 +0900

    중간까지 풀이

---
+++ src/Array/ex6.java
@@ -0,0 +1,34 @@
+package Array;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ex6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int[] arr = new int[a];
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < a; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < a; i++) {
+            int tmp = arr[i];
+           int res = 0;
+           while(tmp>0) {
+                int t = tmp%10;
+                res = res*10 + t;
+                tmp = tmp/10;
+           }
+           result.add(res);
+        }
+
+
+        System.out.print(result);
+
+
+    }
+}
